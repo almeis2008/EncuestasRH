57d6593242d500b0a1978d294494309a4e6d34ee
Aaron Meis <almeis@hotmail.com>
2014-06-04 10:11:35 -0500

    nuevo template inicio de adaptacion vercion aun no funcional

--- EncuestasRh/settings.py
+++ EncuestasRh/settings.py
@@ -107,6 +107,8 @@ ROOT_URLCONF = 'EncuestasRh.urls'
 WSGI_APPLICATION = 'EncuestasRh.wsgi.application'
 
 TEMPLATE_DIRS = (
+    os.path.join(PROJECT_PATH, 'templates')
+
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- EncuestasRh_interface/views.py
+++ EncuestasRh_interface/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+def index(request):
+    return render(request, 'index.html')
